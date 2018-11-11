44869a047f0bd1f7c362f5a62bb0fc6d1f611a68
Kevin Hutchinson <legendum@gmail.com>
2018-11-11 16:36:21 -0600

    First Ad-tech code by AD2.biz

---
+++ utm.js
@@ -0,0 +1,48 @@
+/* utm.js
+ *
+ * Copyright(c) 2018 Legendum Ltd. No Rights Reserved.
+ * This code is Licensed to you under the MIT License.
+ *
+ * 1) Add this line of HTML at the end, before </html> in your landing page(s):
+ *
+ * <script src="ad2.biz/utm.js"></script>
+ *
+ * 2) Add this same line of HTML before </html> in your form submission page.
+ *
+ * 3) Modify the form in your form submission page to add these hidden fields:
+ *
+ * <input type=hidden class=utm name=utm_source>
+ * <input type=hidden class=utm name=utm_medium>
+ * <input type=hidden class=utm name=utm_campaign>
+ * <input type=hidden class=utm name=utm_keyword>
+ * <input type=hidden class=utm name=utm_term>
+ */
+(function(w){
+  var loc=w.location,
+      decode = decodeURIComponent,
+      params = {},
+      fields = [],
+      query = loc.search.substring(1),
+      pairs = query.split('&');
+  for (var i = 0; i < pairs.length; i++) {
+    var pair = pairs[i].split('=');
+    if (pair[0].substr(0, 4)=='utm_') {
+      params[decode(pair[0])] = decode(pair[1]);
+    }
+  }
+  for (var k in params) {
+    w.sessionStorage.setItem(k, params[k]);
+  }
+  fields = w.document.getElementsByClassName('utm');
+  for (var i = 0; i < fields.length; i++) {
+    var f = fields[i],
+        k = f.getAttribute('name'),
+        v = w.sessionStorage.getItem(k);
+    if (v) {
+      f.setAttribute('value', v);
+      params[k] = v; // for debug
+    }
+  }
+  window.AD2=window.AD2||{};
+  window.AD2.utm=params;
+})(window||root)
